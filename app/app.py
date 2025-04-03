5884005365dfeaa49d8df419370f41615da53346
samira-magalhaes <149384728+samira-magalhaes@users.noreply.github.com>
2025-04-03 20:57:47 +0100

    refactor: mudança no código

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 
@@ -7,7 +7,23 @@ app = Flask(__name__)
 
 @app.route("/")    #método rota que faz a página conectar nos links  -  essa barrinha / para indicar que é rota principal
 def home():       # criado função rota da home (por exemplo, hello world)
-    return "Olá, mundo! Aplicação rodando no Docker."
+    # return "Olá, mundo! Aplicação rodando no Docker."
+    return render_template('index.html')
+
+
+@app.route('/user/<username>')
+def show_user(username):
+    return f"Bem-vindo, {username}!"
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+
+# @app.route("/sobre")
+# def sobre():
+#     return render_template('sobre.html')
 
 
 if __name__ == "__main__":
